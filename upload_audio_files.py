a30df3950cb6ef4342894275779ade3ea918de1f
cbandar <61906715+cbandar@users.noreply.github.com>
2023-04-25 02:12:48 +0100

    Update upload_audio_files.py

--- upload_audio_files.py
+++ upload_audio_files.py
@@ -23,15 +23,6 @@ def upload_audio_files(dir):
         bucket.put_object(Key=dir_path[len(dir) + 1 :], Body=data)
 
         print('Uploaded ' + str(file))
-
-        # Publish message to SNS.
-        # message_id = ...
-        message_id = publish_message(
-          'arn:aws:sns:us-east-1:785522309505:sns-s2027892',
-          file,
-          'audio file  upload status'
-        )
-        print(message_id)
         time.sleep(30)
 
 upload_audio_files('audio files')
